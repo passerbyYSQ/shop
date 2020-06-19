76d1f585b037c3977195d838ad3c11bc4ba885db
passerbyYSQ <1127664027@qq.com>
2020-06-19 21:59:03 +0800

    【订单页】堪称完美的JS交互

--- application/index/controller/Cart.php
+++ application/index/controller/Cart.php
@@ -2,20 +2,27 @@
 namespace app\index\controller;
 use think\Controller;
 use app\index\model\Cart as CartModel;
-
+use app\index\model\Goods as GoodsModel;
 /**
  * @author passerbyYSQ
  * @create 2020年6月18日 下午4:21:56
  */
 class Cart extends Controller {
     
+    // 购物车的显示页码
+    public function index() {
+        return view('cart_list');
+    }
+    
     // 异步ajax往购物车里面添加商品
     public function add() { 
         //return json(input('post.'));
-        
+
         // 未登录不允许操作
         if (empty(session('member'))) {
-            return;
+            $res['status'] = false;
+            $res['msg'] = '请先登录';
+            return json($res);
         }
         
         $memberId= session('member.id');
@@ -24,7 +31,6 @@ class Cart extends Controller {
         
         $model = new CartModel();
         $res['status'] = true;
-
         $item = $model->findOne($memberId, $goodsId);
         
         // 购物车中没有
@@ -35,14 +41,20 @@ class Cart extends Controller {
                 $res['msg'] = $item->getError();
             } 
         } else {
-            // 该记录的'count'字段+1
-            if (!$item->setInc('count', $count)) {
+            
+            $goods = GoodsModel::get($goodsId);
+            if ($item->count >= $goods->count) {
                 $res['status'] = false;
-                $res['msg'] = $item->getError();
+                $res['msg'] = '购物车中的商品数量超出现有库存';
+            } else {
+                // 该记录的'count'字段+1
+                if (!$item->setInc('count', $count)) {
+                    $res['status'] = false;
+                    $res['msg'] = $item->getError();
+                }
             }
         }
         return json($res);
     }
-
 }
 ?>
